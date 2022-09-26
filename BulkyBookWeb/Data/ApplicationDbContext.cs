420f3e298fae180d9d152f89ba41307ea83bbec2
MasriNaser <Naser.Masri@cronos.be>
2022-09-26 10:15:45 +0200

    oobj added

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        //contstrcture for DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+    
+}
+
--- BulkyBookWeb/wwwroot/js/site.js
+++ BulkyBookWeb/wwwroot/js/site.js
@@ -3,3 +3,36 @@
 
 // Write your JavaScript code.
 
+const add = (n1 = 1, n2 = 1) => n1 + n2;
+
+console.log(add(5, 5));
+
+var myObject = { id: 1, text: "this is", isComplete: true };
+
+Object.keys(myObject).reduce(function (key, index) {
+    myObject[key];
+});
+
+console.log(myObject);
+
+const object1 = {
+    id: 1,
+    text: "this is a text",
+    isCompeleted: true,
+};
+
+for (const [key, value] of Object.entries(object1)) {
+
+    console.log(`${key}: ${value}`);
+}
+//constraction
+
+function Car(model, color, create) {
+    this.model = model;
+    this.color = color;
+    this.create = create;
+}
+
+const car1 = new Car("Opel", "Red", "12-3-2020");
+
+console.log(car1);
